2c5037a0abf6c79fe2c54f7927b77f5ac3ac31f8
Ueli Haltner <ul528383@dal.ca>
2019-07-27 15:46:47 -0300

    Update INSincController.js

--- WebService/api/controllers/INSincController.js
+++ WebService/api/controllers/INSincController.js
@@ -48,7 +48,7 @@ module.exports = {
             var deductableValue = calculateDeductableAmount(appraisalValue);
             
             //create the MBRWebURL string with parameters
-            var MbrWebUrl = "https://webservicenimbus.herokuapp.com/mbr/insuranceUpdate/?";
+            var MbrWebUrl = "https://webservicenimbus.herokuapp.com/insuranceUpdate/?";
             MbrWebUrl = MbrWebUrl + "MortID="+mortID+"&";
             MbrWebUrl = MbrWebUrl + "MlsID="+mlsID+"&";
             MbrWebUrl = MbrWebUrl + "FullName="+fullname+"&";
